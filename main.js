3e7255267b42438a10dd3a73dbe397529097e0c7
arnold <62089140+central182@users.noreply.github.com>
2023-01-18 00:23:48 +0900

    Make the extension work

---
+++ main.js
@@ -0,0 +1,47 @@
+(async () => {
+  await reiteratePreferenceOnPageLanguage();
+})();
+
+// If the current page is written in one of the preferred languages,
+// set the page language as the ONLY (and consequently, the FIRST) preferred language.
+//
+// There are some translation scripts that
+// respect only the FIRST preferred language and disregard the others.
+// The following function is to prevent them from aggressively attempting to
+// translate a less-preferred language into the FIRST preferred language.
+async function reiteratePreferenceOnPageLanguage() {
+  const pageLanguage = getPageLanguage();
+  const preferredLanguages = await getPreferredLanguages();
+  if (preferredLanguages.has(pageLanguage)) {
+    expressPreferenceOn(pageLanguage);
+  }
+}
+
+function normaliseLanguage(languageTag) {
+  try {
+    return new Intl.Locale(languageTag).language;
+  } catch (error) {
+    return "";
+  }
+}
+
+function getPageLanguage() {
+  return normaliseLanguage(document.documentElement.lang);
+}
+
+async function getPreferredLanguages() {
+  return new Set(
+    (await browser.i18n.getAcceptLanguages())
+      .map((x) => normaliseLanguage(x))
+      .filter((x) => x !== "")
+  );
+}
+
+function expressPreferenceOn(language) {
+  Object.defineProperty(window.navigator.wrappedJSObject, "languages", {
+    value: [language],
+  });
+  Object.defineProperty(window.navigator.wrappedJSObject, "language", {
+    value: language,
+  });
+}
